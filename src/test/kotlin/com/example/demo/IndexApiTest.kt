56daf74577ed1360648f37292e78acdc55677129
holdonnn <rururu0729@gmail.com>
2020-04-11 23:17:48 +0900

    Add first test case

---
+++ src/test/kotlin/com/example/demo/IndexApiTest.kt
@@ -0,0 +1,32 @@
+package com.example.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+
+import org.hamcrest.Matchers.containsString
+import org.junit.jupiter.api.DisplayName
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+internal class IndexApiTest {
+
+    @Autowired
+    lateinit var mockMvc: MockMvc
+
+    @Test
+    @DisplayName("hello world!")
+    fun test() {
+        this.mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello")))
+    }
+}
